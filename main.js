f123c45bac94e93f72e2481652fbf2730ce2b854
Camille-Durand <camillefanguin@gmail.com>
2023-11-28 17:04:08 +0100

    try random

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 Vue.createApp({
     data() {
         return {
-            
+            random: ''
         };
+    },
+    methods: {
+        randomTxt() {
+            let randomness = Math.floor(Math.random() * 3);
+            switch(randomness) {
+                case 1:
+                    this.random = "Hey i'm test 1";
+                    break;
+                case 2:
+                    this.random = "Heyo i'm test 2";
+                    break;
+                case 3:
+                    this.random = "Hello i'm test 3";
+                    break;
+                default:
+                    this.random = "You are not supposed to see me. If you do, it's a bug"
+                    break;
+            }
+        }
     }
 }).mount('#app');

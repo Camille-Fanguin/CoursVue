dfa144ecd02d49f6f47957eb5ecef5dd24db899a
Camille-Durand <camillefanguin@gmail.com>
2023-11-29 14:21:40 +0100

    v-on:events

--- main.js
+++ main.js
@@ -3,5 +3,20 @@ Vue.createApp({
         return {
             
         };
+    },
+    methods: {
+        tapEvent() {
+            para = document.querySelectorAll('p');
+            input = document.querySelectorAll('input');
+            para[0].textContent = input[0].value;
+        },
+        echapEvent() {
+            para = document.querySelectorAll('p');
+            input = document.querySelectorAll('input');
+            para[1].textContent = input[1].value;
+        },
+        alertMsg() {
+            return alert("Petite alerte: Hello World");
+        }
     }
 }).mount('#app');

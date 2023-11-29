7e9a889f2bf41948c01c43533f44ac91bc97694e
Camille-Durand <camillefanguin@gmail.com>
2023-11-29 09:30:27 +0100

    Code random txt

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 Vue.createApp({
     data() {
         return {
-            random: ''
+            text: 'test'
         };
     },
     methods: {
         randomTxt() {
-            let randomness = Math.floor(Math.random() * 3);
+            let randomness = Math.floor(Math.random() * 4);
             switch(randomness) {
+                case 0:
+                    this.text = "I'm test 0";
+                    break;
                 case 1:
-                    this.random = "Hey i'm test 1";
+                    this.text = "Hey i'm test 1";
                     break;
                 case 2:
-                    this.random = "Heyo i'm test 2";
+                    this.text = "Heyo i'm test 2";
                     break;
                 case 3:
-                    this.random = "Hello i'm test 3";
+                    this.text = "Hello i'm test 3";
                     break;
                 default:
-                    this.random = "You are not supposed to see me. If you do, it's a bug"
+                    this.text = "You are not supposed to see me. If you do, it's a bug"
                     break;
             }
+            return this.text;
         }
     }
 }).mount('#app');

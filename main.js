ad050dad70c300c32ddc21e40581f164fa85b1b5
Camille-Durand <camillefanguin@gmail.com>
2023-12-04 14:36:25 +0100

    code input1, input2 & button

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 Vue.createApp({
     data() {
         return {
-            
+            hello: false,
+            world: false,
+
+            text1: '',
+            text2: '',
+
+            stateButton: false
         };
+    },
+    methods: {
+        getFirstInput() {
+            if(this.text1 == 'Hello') {
+                this.hello = true;
+            } else if (this.text1 == 'World') {
+                this.world = true;
+            } else {
+                this.hello = false;
+                this.world = false;
+            }
+        },
+        buttonHide() {
+            console.log(this.stateButton);
+            this.stateButton = !this.stateButton;
+        }
     }
 }).mount('#app');

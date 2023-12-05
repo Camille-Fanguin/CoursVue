d06a7fe2a9480ac95b03cdc33116c79e64c9f32d
Camille-Durand <camille.durand@limayrac.fr>
2023-12-05 09:46:23 +0100

    récup exo précédent

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 Vue.createApp({
     data() {
         return {
-
+            mesFilms: [],
+            nomFilm: '',
         };
+    },
+    methods: {
+        addMovie() {
+            return this.mesFilms.push(this.newMovie);
+        }
     }
 }).mount('#app');

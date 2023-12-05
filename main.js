9cd079e9f68247039b6c21360a3f79f0b308a84c
Camille-Durand <camille.durand@limayrac.fr>
2023-12-05 11:52:28 +0100

    list courses + hide list

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 Vue.createApp({
     data() {
         return {
+            listCourses: [],
+            article: '',
 
+            showing: false
         };
+    },
+    methods: {
+        addArticle() {
+            return this.listCourses.push(this.article);
+        },
+        showingList() {
+            return this.showing = !this.showing;
+        }
     }
 }).mount('#app');

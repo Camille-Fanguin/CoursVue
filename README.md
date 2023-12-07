5106000fb69233ae57f76071db204a1b50ca68fd
Camille-Durand <camillefanguin@gmail.com>
2023-12-07 10:08:06 +0100

    code 2 cards

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 
-CDA: 
+# ComponentsVue
--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 Vue.createApp({
     data() {
         return {
+            detailsVisibles: false,
             
+            lesAmis: [{
+                id: '1',
+                name: 'Test1',
+                phone: '01 23 45 67 89',
+                email: 'helloImTest@gmail.com'
+            },{
+                id: '2',
+                name: 'Test2',
+                phone: '09 87 65 43 21',
+                email: 'helloImTestToo@gmail.com'
+            }],
         };
+    },
+    methods: {
+        toggleDetails() {
+            return this.detailsVisibles = !this.detailsVisibles;
+        }
     }
 }).mount('#app');
